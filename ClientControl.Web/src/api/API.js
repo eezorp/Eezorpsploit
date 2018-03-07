3f7cdf945aab9f1adb2d772b151ec2a7fc6fd916
NickProzee_Development@outlook.com <nickprozee_development@outlook.com>
2018-03-07 10:18:15 +0100

    temp checkin

--- ClientControl.Web/src/api/API.js
+++ ClientControl.Web/src/api/API.js
@@ -1,5 +1,7 @@
-const clients_url = "http://10.188.4.40:8000/clients";
-const shellcode_url = "http://10.188.4.40:8000/shellcode";
+//const base = "localhost";
+const base = "192.168.178.14";
+const clients_url = "http://"+base+":8000/clients";
+const shellcode_url = "http://"+base+":8000/shellcode";
 
 
 export function getShellCodes() {
--- ClientControl.Web/src/components/ShellModal.js
+++ ClientControl.Web/src/components/ShellModal.js
@@ -6,7 +6,18 @@ import Dialog, {
     DialogContent,
     DialogTitle,
 } from 'material-ui/Dialog';
+
+import RadioButton, {RadioGroup} from 'material-ui/Radio';
 import Input from 'material-ui/Input';
+
+const styles = {
+    block: {
+        maxWidth: 250,
+    },
+    radioButton: {
+        marginBottom: 16,
+    },
+};
 
 class ShellModal extends React.Component {
     constructor(props) {
@@ -35,27 +46,38 @@ class ShellModal extends React.Component {
                 <Dialog
                     open={this.props.isOpen}
                     onClose={this.props.toggle}
-                    fullWidth
-                >
+                    fullWidth>
 
-                    <DialogTitle id="alert-dialog-title">{"Insert ShellCode"}</DialogTitle>
+                    <RadioGroup name="shipSpeed" defaultSelected="not_light">
+                        <RadioButton
+                            value="light"
+                            label="Simple"
+                            style={styles.radioButton}
+                        />
+                        <RadioButton
+                            value="not_light"
+                            label="Selected by default"
+                            style={styles.radioButton}
+                        /></RadioGroup>
 
-                    <DialogContent>
-                        <Input value={this.state.shellcode} onChange={ev => this.setShellCode(ev.target.value)} multiline fullWidth defaultValue="Paste ShellCode in here"></Input>
-                    </DialogContent>
+                        <DialogTitle id="alert-dialog-title">{"Insert ShellCode"}</DialogTitle>
 
-                    <DialogActions>
-                        <Button key={1} onClick={() => {
-                            this.props.onInsert(this.state.shellcode);
-                            this.props.toggle();
-                        }} color="primary" autoFocus>Insert</Button>
-                        <Button key={2} onClick={this.props.toggle} color="primary">Cancel</Button>
+                        <DialogContent>
+                            <Input value={this.state.shellcode} onChange={ev => this.setShellCode(ev.target.value)} multiline fullWidth defaultValue="Paste ShellCode in here"></Input>
+                        </DialogContent>
 
-                    </DialogActions>
+                        <DialogActions>
+                            <Button key={1} onClick={() => {
+                                this.props.onInsert(this.state.shellcode);
+                                this.props.toggle();
+                            }} color="primary" autoFocus>Insert</Button>
+                            <Button key={2} onClick={this.props.toggle} color="primary">Cancel</Button>
+
+                        </DialogActions>
                 </Dialog>
             </div>
-        );
-    }
-}
-
+                );
+            }
+        }
+        
 export default ShellModal;
